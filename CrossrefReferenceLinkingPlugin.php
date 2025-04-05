678d9a026337ca4d5e664fff0af047c9f74d8081
Alec Smecher <alec@smecher.bc.ca>
2025-04-04 17:28:34 -0700

    #28 Address PHP log warnings

--- CrossrefReferenceLinkingPlugin.php
+++ CrossrefReferenceLinkingPlugin.php
@@ -88,7 +88,7 @@ class CrossrefReferenceLinkingPlugin extends GenericPlugin
     {
         // If crossref plugin is set i.e. the crossref credentials exist we can assume that DOI plugin is set correctly
         $crossrefPlugin = PluginRegistry::getPlugin('generic', 'crossrefplugin');
-        return $crossrefPlugin && strlen($crossrefPlugin->getSetting($contextId, 'username')) > 0 && strlen($crossrefPlugin->getSetting($contextId, 'password')) > 0;
+        return $crossrefPlugin && strlen((string) $crossrefPlugin->getSetting($contextId, 'username')) > 0 && strlen((string) $crossrefPlugin->getSetting($contextId, 'password')) > 0;
     }
 
     /**
